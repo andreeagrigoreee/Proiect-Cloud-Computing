aedacf0f9b3cb5ccecccd3916da7dd2a18c79969
andreea <grigoreandreea18@stud.ase.ro>
2022-05-14 23:21:29 +0300

    add router and routes

---
+++ Rute/messages-router.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const mysql=require("mysql");
+const router = express.Router();
+const connection = require('../db-conn'); 
+
+// Get all messages 
+router.get("/",(req,res)=>{
+    connection.query("SELECT * FROM messages", (err,results)=>{
+        if(err){
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            messages: results
+        })
+    })
+});
+
+//Get a message by id 
+router.get("/:id",(req,res)=>{
+    const { id } = req.params;
+    connection.query(`SELECT * FROM messages where entryID=${mysql.escape(id)}`, (err,results)=>{
+        if(err){
+            console.log(err);
+            return res.send(err);
+        }
+
+        if(results.length===0){
+            return res.status(400).json({
+                error:"Message not found",
+            })
+        }
+        return res.json({
+            messages: results
+        })
+    })
+});
+
+//Insert a message 
+router.post("/",(req,res)=>{
+    console.log(req.body);
+    const {
+        senderName,
+        senderMail,
+        receiverMail,
+        messageContent,
+    } = req.body;
+
+    if(!senderMail||!senderName||!receiverMail||!messageContent)
+    {
+        return res.status(400).json({
+            error:"All fields are requiered"
+        })
+    }
+
+    connection.query(`insert into messages(senderName,senderMail,receiverMail,messageContent)    values (${mysql.escape(senderName)},${mysql.escape(senderMail)},${mysql.escape(receiverMail)},${mysql.escape(messageContent)})`, (err,results)=>{
+        if(err){
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+             results
+        })
+    })
+});
+
+//Delete a message 
+router.delete("/:id",(req,res)=>{
+    const { id } = req.params;
+    connection.query(`DELETE FROM messages where entryID=${mysql.escape(id)}`, (err,results)=>{
+        if(err){
+            console.log(err);
+            return res.send(err);
+        }
+
+        
+        return res.json({
+             results,
+        })
+    })
+})
+
+
+//Update a message 
+router.put("/:id",(req,res)=>{
+    const { id } = req.params;
+    const {
+        senderName,
+        senderMail,
+        receiverMail,
+        messageContent,
+    } = req.body;
+
+    if(!senderMail||!senderName||!receiverMail||!messageContent)
+    {
+        return res.status(400).json({
+            error:"All fields are requiered"
+        })
+    }
+
+    connection.query(`UPDATE  messages SET  senderName=${mysql.escape(senderName)},senderMail=${mysql.escape(senderMail)},receiverMail=${mysql.escape(receiverMail)},messageContent=${mysql.escape(messageContent)}`, (err,results)=>{
+        if(err){
+            console.log(err);
+            return res.send(err);
+        }
+
+        
+        return res.json({
+             results,
+        })
+    })
+})
+module.exports = router;
+
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const messagesRouter = require('./Rute/messages-router');
+
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use("/messages",messagesRouter);
+
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.send('Hello World again!');
 });
+ */
+
+
 
 app.listen(port, () => {
   console.log(`Example app LOL listening on port ${port}!`)
